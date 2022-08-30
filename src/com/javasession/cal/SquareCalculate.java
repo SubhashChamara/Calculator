e2e88e5985ba232f5fd2a9a5336314fefe6c3b70
Subhash Chamara <udayanga.subhash94@gmail.com>
2022-08-30 23:12:13 +0530

    debug semicolloan missing

---
+++ src/com/javasession/cal/SquareCalculate.java
@@ -0,0 +1,29 @@
+package com.javasession.cal;
+
+public class SquareCalculate {
+    public int getHight() {
+        return hight;
+    }
+
+    public void setHight(int hight) {
+        this.hight = hight;
+    }
+
+    private int hight;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    private int width;
+    public int squar(int hight,int width) {
+        return hight * width;
+
+
+    }
+
+}
